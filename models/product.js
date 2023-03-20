d8ef97cfbb6b6169d1b5eb57b54e7e8d07c39505
manzar0911 <manzarhamidi0911@gmail.com>
2023-03-20 22:58:22 +0530

    host api

---
+++ models/product.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+import { APP_URL } from '../config';
+
+const productSchema = new Schema(
+    {
+        title: { type: String, required: true },
+        desc: { type: String, required: true },
+        age: { type: Number, required: true },
+        price: { type: Number, required: true },
+        phone: {type: Number, required: true},
+        image: {
+            type: String,
+            required: true,
+            get: (image) => {
+                // http://localhost:5000/uploads/1616443169266-52350494.png
+                // if (process.env.ON_HEROKU == 'true') {
+                //     return `${image}`;
+                // }
+                return `${APP_URL}/${image}`;
+            },
+        },
+    },
+    { timestamps: true, toJSON: { getters: true }, id: false }
+);
+
+export default mongoose.model('Product', productSchema, 'products');
